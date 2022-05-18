a88c038a12982d7edc03c8c2a623c4fc5dcee31b
DanilNaum <90374685+DanilNaum@users.noreply.github.com>
2022-05-18 13:35:13 +0300

    Гаус работает, необходим рефакторинк (повторение кода)+ не добавлено исключение, первый элемент строки равен 0

--- numeral_methods_2/main.cpp
+++ numeral_methods_2/main.cpp
@@ -4,12 +4,15 @@
 #include <iomanip>
 
 using namespace std;
-
+/*vector<vector<double>> ForwardGauss(vector<vector <double>>& A, vector<double>& B) {
+	
+}
+*/
 vector<double>Gaus(vector<vector <double>>& A, vector<double>& B) {
-	for (int i = 0; i < A.size(); i++) {
+	for (int i = 0; i < A.size(); i++) { //прямой ход Гауса
 		double first = A[i][i];
 		B[i] /= first;
-		for (int j = 0; j < A[i].size(); j++) {
+		for (int j = 0; j < A[i].size(); j++) { 
 			A[i][j] /= first;
 			
 		}
@@ -21,20 +24,52 @@ vector<double>Gaus(vector<vector <double>>& A, vector<double>& B) {
 				A[k][j] -= A[i][j] * alpha;
 			}B[k] -= alpha*B[i];
 		}
+	}
+	
+	reverse(A.begin(), A.end());			//переворот массив, эмитация обратного хода гауса 
+	reverse(B.begin(), B.end());
+	for (int i = 0; i < A.size(); i++) {
+		reverse(A[i].begin(), A[i].end());
+	}
+	for (int i = 0; i < A.size(); i++) {
+		double first = A[i][i];
+		B[i] /= first;
+		for (int j = 0; j < A[i].size(); j++) {
+			A[i][j] /= first;
 
+		}
+		for (int k = i + 1; k < A.size(); k++) {
+			double alpha = 0;
+			for (int j = 0; j < A[i].size(); j++) {
+				if (alpha == 0)
+					alpha = A[k][i];
+				A[k][j] -= A[i][j] * alpha;
+			}B[k] -= alpha * B[i];
+		}
+	}
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+	for (int i = 0; i < A.size(); i++) {
+		reverse(A[i].begin(), A[i].end());
 	}
 	for (int i = 0; i < A.size(); i++) {
 		for (int j = 0; j < A[i].size(); j++) {
 			cout<<A[i][j]<<" ";
 		}
-		cout << B[i] << endl;
+		cout <<" \t |" << B[i] << endl;
 	}
 	return B;
 }
 
 int main() {
-	vector<vector<double>>A = { {13., 1., 1.} ,{1.,15.,1.} ,{1., 1., 17.} };
+	vector<vector<double>>A = { {13., 1., 1.} 
+	                           ,{1., 15., 1.} 
+	                           ,{1., 1., 17.} };
 	vector<double>B = { 15., 17., 19. };
-	Gaus(A, B);
+	vector<double>ans = Gaus(A, B);
+	cout << "Gauss:" << " ";
+	for (int i = 0; i < ans.size(); i++) {
+		cout << ans[i] << " ";
+	}
 	return 0;
 }
